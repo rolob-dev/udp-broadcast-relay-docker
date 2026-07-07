78c604db6b41e0a5c5042d989343c129ff1fc962
Roland Obrist <r.obrist@gmx.ch>
2026-07-07 19:14:15 +0200

    Add interface diagnostics

--- internal/network/network.go
+++ internal/network/network.go
@@ -1,6 +1,7 @@
 package network
 
 import (
+	"fmt"
 	"net"
 
 	"github.com/rolob-dev/udp-broadcast-relay-docker/internal/config"
@@ -13,36 +14,50 @@ func Discover(cfg *config.Config) error {
 		return err
 	}
 
-	for i := range cfg.Networks {
+	fmt.Println("Detected interfaces")
+	fmt.Println()
 
-		for _, iface := range interfaces {
+	for _, iface := range interfaces {
 
-			addrs, err := iface.Addrs()
-			if err != nil {
+		fmt.Printf("%s\n", iface.Name)
+		fmt.Printf("    Index : %d\n", iface.Index)
+		fmt.Printf("    MTU   : %d\n", iface.MTU)
+		fmt.Printf("    Flags : %s\n", iface.Flags.String())
+
+		addrs, err := iface.Addrs()
+		if err != nil {
+			fmt.Println()
+			continue
+		}
+
+		if len(addrs) == 0 {
+			fmt.Println("    Address: (none)")
+		}
+
+		for _, addr := range addrs {
+
+			ipnet, ok := addr.(*net.IPNet)
+			if !ok {
 				continue
 			}
 
-			for _, addr := range addrs {
+			fmt.Printf("    Address: %s\n", ipnet.String())
 
-				ipnet, ok := addr.(*net.IPNet)
-				if !ok {
-					continue
-				}
+			// Nur IPv4 für unsere Zuordnung verwenden
+			if ipnet.IP.To4() == nil {
+				continue
+			}
 
-				// Nur IPv4
-				if ipnet.IP.To4() == nil {
-					continue
-				}
+			for i := range cfg.Networks {
 
-				// Liegt die IP in unserem konfigurierten Netz?
 				if cfg.Networks[i].CIDR.Contains(ipnet.IP) {
-
 					cfg.Networks[i].Interface = &iface
 					cfg.Networks[i].Addresses = append(cfg.Networks[i].Addresses, ipnet)
-
 				}
 			}
 		}
+
+		fmt.Println()
 	}
 
 	return nil
