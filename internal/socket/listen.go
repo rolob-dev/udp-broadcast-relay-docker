78bff8b6abcda292b3608c8b9dcd90ccbd4ecfd6
Roland Obrist <r.obrist@gmx.ch>
2026-07-07 21:05:46 +0200

    Log received SSDP packets

--- internal/socket/listen.go
+++ internal/socket/listen.go
@@ -22,6 +22,11 @@ func (m *Manager) Listen(interfaces map[string]*net.Interface) error {
 		fmt.Printf("Source    : %s\n", pkt.Source)
 		fmt.Printf("Target    : %s\n", pkt.Control.Dst)
 		fmt.Printf("Length    : %d bytes\n", pkt.Length)
+		fmt.Println()
+		fmt.Println("----------------------------------------")
+		fmt.Println()		
+		fmt.Println(string(pkt.Data))
+		fmt.Println()		
 		fmt.Println("----------------------------------------")
 		fmt.Println()
 
